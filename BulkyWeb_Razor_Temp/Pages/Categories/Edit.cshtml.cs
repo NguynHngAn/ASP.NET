e692eeaa5dd4087557bf3af50308d1d0fa8bbc9d
Hong An <nguyenhongan.tphcm@gmail.com>
2024-08-01 17:41:25 +0700

    First Upload Code

---
+++ BulkyWeb_Razor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,34 @@
+using BulkyWeb_Razor_Temp.Data;
+using BulkyWeb_Razor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWeb_Razor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if(id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+        public IActionResult OnPost()
+        {
+
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToPage("Index");
+
+        }
+    }
+}
